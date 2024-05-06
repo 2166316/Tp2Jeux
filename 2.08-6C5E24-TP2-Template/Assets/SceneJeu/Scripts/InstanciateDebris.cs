008112cb1b711ca2d0bba1fa6f63c7d14877bd43
Aemir <49481822+QIpug@users.noreply.github.com>
2024-05-06 15:49:33 -0400

    ajout des spectateurs

--- 2.08-6C5E24-TP2-Template/Assets/SceneJeu/Scripts/InstanciateDebris.cs
+++ 2.08-6C5E24-TP2-Template/Assets/SceneJeu/Scripts/InstanciateDebris.cs
@@ -6,7 +6,9 @@ using UnityEngine.Networking;
 public class InstanciateDebris : NetworkBehaviour
 {
     [SerializeField] public GameObject bacPoubelle;
+    [SerializeField] public GameObject spectateur;
     [SerializeField] private List<Vector3> listDePositionPredefinie;
+    [SerializeField] private List<Vector3> listDePositionSpectateur;
     public override void OnNetworkSpawn()
     {
        // Debug.Log("spawn");
@@ -20,10 +22,20 @@ public class InstanciateDebris : NetworkBehaviour
             new Vector3(-240.91f, 69.86519f, 64.68f)
         };
 
+        listDePositionSpectateur = new List<Vector3>
+        {
+            new Vector3(-344.846f, 69.66448f, 16.99996f),
+            new Vector3(-348.846f, 69.66448f, 16.99996f),
+            new Vector3(-354.646f, 69.66448f, 16.99996f),
+            new Vector3(-344.846f, 69.66448f, 3f),
+            new Vector3(-348.846f, 69.66448f, 3f),
+            new Vector3(-354.646f, 69.66448f, 3f)
+        };
 
         if (IsHost)
         {
             InstantiePoubelles();
+            InstantieSpectateurs();
         }    
     }
 
@@ -42,6 +54,31 @@ public class InstanciateDebris : NetworkBehaviour
                
             }
             
+        }
+    }
+
+    private void InstantieSpectateurs()
+    {
+        int i = 0;
+        foreach (Vector3 t in listDePositionSpectateur)
+        {
+            i++;
+            GameObject spectateurObj = null;
+            if (i > 3)
+            {
+               spectateurObj = Instantiate(spectateur, t, new Quaternion(0f, 0f, 0f, 0f));
+            }
+            else
+            {
+                spectateurObj = Instantiate(spectateur, t, new Quaternion(0f, 180f, 0f, 0f));
+            }
+
+            NetworkObject networkObject = spectateurObj.GetComponent<NetworkObject>();
+            if (networkObject != null)
+            {
+                networkObject.Spawn();
+            }
+
         }
     }
 
