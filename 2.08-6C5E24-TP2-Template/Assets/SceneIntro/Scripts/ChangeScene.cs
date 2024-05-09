524702e8fb4133ef4e361cd27ed0088741bee75b
Aemir <49481822+QIpug@users.noreply.github.com>
2024-05-09 11:31:24 -0400

    Ajout du script de despawn

--- 2.08-6C5E24-TP2-Template/Assets/SceneIntro/Scripts/ChangeScene.cs
+++ 2.08-6C5E24-TP2-Template/Assets/SceneIntro/Scripts/ChangeScene.cs
@@ -1,10 +1,32 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using System;
+using System.Linq;
+using Unity.Netcode;
+using UnityEngine;
 
-public class ChangeScene : MonoBehaviour
+public class ChangeScene : NetworkBehaviour
 {
-    public void LoadScene(string name) {
-//        Debug.Log("Scene : " + name);
-        SceneManager.LoadScene(name);
+    public void LoadScene(string name)
+    {
+        try
+        {
+            TestServerRPC();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void TestServerRPC(ServerRpcParams serverRpcParams = default)
+    {
+        var clientId = serverRpcParams.Receive.SenderClientId;
+        Debug.Log("id : " + clientId);
+        NetworkObject clientPickup = NetworkManager.Singleton.ConnectedClients.Values.ToList().FirstOrDefault(n => n.ClientId == clientId).PlayerObject;
+        if (clientPickup != null)
+        {
+            NetworkManager.Singleton.DisconnectClient(clientId);
+            clientPickup.Despawn();
+        }
     }
 }
